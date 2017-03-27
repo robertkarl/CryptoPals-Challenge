aebe5252fefa687437caf95a2ab7393b066d64db
Robert Karl <robertkarljr@gmail.com>
2017-03-26 22:04:02 -0700

    Messing with repkey thingy

--- base64_repkey_xor.c
+++ base64_repkey_xor.c
@@ -4,7 +4,7 @@
 #include "common.h"
 
 void usage(char **argv) {
-	printf("usage: %s plaintext rotatingcipher\n", argv[0]);
+	printf("usage: %s base64 rotatingcipher\n", argv[0]);
 	exit(-1);
 }
 
@@ -20,16 +20,19 @@ void repkey_xor(char *in, int clearlen, char *out, char *key, int keylen)
 int main(int argc, char **argv) {
 	int len;
 	char *cypher, *coded;
+	uint8_t *raw;
+	int rawlen;
+
 	if (argc != 3)
 		usage(argv);
 	len = strlen(argv[1]);
-	cypher = malloc(len);
-	coded = malloc(2 * len);
 
-	repkey_xor(argv[1], strlen(argv[1]), cypher, argv[2], strlen(argv[2]));
-	data2hex(cypher, coded, len);
-	printf("%s\n", coded);
-	
+	rawlen = 3 * len / 4;
+	raw = malloc(rawlen);
+	cypher = malloc(rawlen);
+	base64_to_data(argv[1], raw);
+	repkey_xor(raw, rawlen, cypher, "ioiio", 5);
+	printf("%s\n", cypher);
 	return -1;
 }
 
